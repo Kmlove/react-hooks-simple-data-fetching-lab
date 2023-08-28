5b0c9b87a114c726bb41f42cc26091bb2b0f6331
Kim Love <kimberlymlove15@gmail.com>
2023-08-28 17:37:37 -0400

    All tests passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
+import { useEffect, useState } from "react"
+
 // create your App component here
+function App (){
+    const [image, setImage] = useState(null)
+    const [loading, isLoading] = useState(true)
+    
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            setImage(data.message)
+            isLoading(false)
+        })
+    }, [])
+
+    if(loading){
+        return <p>Loading...</p>
+    }
+
+    return(
+        <img src={image} alt="A Random Dog"/>
+    )
+
+}
+
+export default App
